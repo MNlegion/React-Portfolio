7a83546b5612187c44e66ca4cb6d3679d7f4ea4f
MNlegion <rkreuser30@gmail.com>
2022-09-22 15:48:12 -0400

    initial setup

---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,23 @@
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+import adidas from '../assets/adidas.png';
+import amazon from '../assets/amazon.png';
+import asus from '../assets/asus.png';
+
+import bgIMG from '../assets/bgIMG.png';
+import bgWhite from '../assets/bgWhite.png';
+
+export default {
+    about01,
+    about02,
+    about03,
+    about04,
+    adidas,
+    amazon,
+    asus,
+    bgIMG,
+    bgWhite
+}
